ad35a4f4c03633f7d4e67803a7fcf8ae9f5ecd77
Márcio miranda <m-andremiranda@outlook.com>
2020-04-07 02:04:55 -0300

    ad

--- Entity/ContaCorrente.php
+++ Entity/ContaCorrente.php
@@ -2,10 +2,10 @@
 
 class ContaCorrente
 {
-    public $titular;
+    private $titular;
     public $agencia;
-    public $numero;
-    public $saldo;
+    private $numero;
+    private $saldo;
 
     public function __construct($titular, $agencia, $numero, $saldo)
     {
@@ -13,6 +13,40 @@ class ContaCorrente
         $this->agencia = $agencia;
         $this->numero = $numero;
         $this->saldo = $saldo;
+    }
+
+    public function sacar($valor)
+    {
+        $this->saldo = $this->saldo - $valor;
+
+        #É uma boa prática retornar a própria classe com 
+        #"return $this" em uma classe que contém métodos 
+        #que não retornam nada. Isso é bom para encadiar métodos
+        # como nesse exemplo abaixo
+        #$object = new Class_Name();
+        #$object->setField1($value1)->setField2($value2)->setField3($value3); 
+        return $this;
+    }
+
+    public function depositar($valor)
+    {
+        $this->saldo = $this->saldo + $valor;
+        return $this;
+    }
+
+    public function getTitular()
+    {
+        return $this->titular;
+    }
+
+    public function getSaldo()
+    {
+        return $this->saldo;
+    }
+
+    public function setNumero($numero)
+    {
+        return $this->numero = $numero;
     }
 
 }
--- Entity/index.php
+++ Entity/index.php
@@ -2,6 +2,10 @@
 
 require_once "ContaCorrente.php";
 
-$contaJoao = new ContaCorrente("Joao", "1212", "342234-2", 1.000,00);
+$contaJoao = new ContaCorrente("Joao", "1212", "342234-2", 1000.00);
 
-var_dump($contaJoao);
+echo $contaJoao->setNumero("44444");
+
+$contaJoao->sacar(200,00)->depositar(300,00);
+
+#var_dump($contaJoao);
